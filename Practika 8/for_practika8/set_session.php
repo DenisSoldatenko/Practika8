0478d06c25ee518c96b9b53e445c6085b4afad39
DenisSoldatenko <91652936+DenisSoldatenko@users.noreply.github.com>
2021-11-14 13:42:26 +0200

    Add files via upload

---
+++ Practika 8/for_practika8/set_session.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+
+<section>
+    <?php
+    $_SESSION["name"] = "Vasia";
+    echo "Session variables are set.";
+    ?>
+</section>
+
+<style>
+    section {
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: column;
+    }
+</style>
